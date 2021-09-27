47572c44b825a3f2bd8d5e92ca3a2398eba5b059
julianasalazar <limajuliana115@yahoo.com.br>
2021-09-27 16:01:28 -0300

    Primeiro desafio do Bootcamp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CadastrodeSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
